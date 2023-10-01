1bba55ec5e8d8967ee1e66a2ba8f309e64107734
khuong tran <khuongthcstanhung@gmail.com>
2023-10-01 08:57:14 +0700

    done

---
+++ src/services/api.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const API_KEY = '3de15395576f253a406faee37e3f9e9a';
+const API_URL = 'http://api.openweathermap.org/data/2.5/weather';
+
+export const getWeather = async (city, country) => {
+    try {
+        let response = await axios.get(`${API_URL}?q=${city},${country}&appid=${API_KEY}&units=metric`);
+        return response.data;
+    } catch (error) {
+        console.log('Error while calling the api ', error.message);
+        return error.response;
+    }
+}
